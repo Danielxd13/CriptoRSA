414353fc52ad0a3a9bd28809aae3f18c5275f578
Danielxd13 <dsuarezsoto2000@gmail.com>
2024-11-15 14:36:42 +0100

    Resultado de la firma rsa en hexadecimal

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,7 @@
 import rsa
+
 def encrypt_rsa(text, exponent=65537, key_length=2048):
     # Generate an RSA key with the specified length and exponent
-    """
-    Encrypts a given text using RSA encryption.
-
-    Parameters:
-        text (str): The text to encrypt
-        exponent (int): The exponent to use for the RSA key (default: 65537)
-        key_length (int): The length of the RSA key in bits (default: 2048)
-
-    Returns:
-        tuple: A tuple containing the encrypted text and the public key
-    """
     (public_key, private_key) = rsa.newkeys(key_length, exponent)
 
     # Encrypt the text using the public key and private key
@@ -20,13 +10,16 @@ def encrypt_rsa(text, exponent=65537, key_length=2048):
     return encrypted_text, public_key, private_key
 
 def sign_message(message, private_key):
-    # convertir la clave privada a formato PEM
+    # Convert the private key to PEM format
     private_key_pem = private_key.save_pkcs1()      
 
-    # cargar la clave privada en formato PEM
+    # Load the private key from the PEM format
     private_key_loaded = rsa.PrivateKey.load_pkcs1(private_key_pem) 
 
-    return rsa.pkcs1.sign(message.encode(), private_key_loaded, 'SHA-256')
+    # Sign the message using the private key
+    signature = rsa.pkcs1.sign(message.encode(), private_key_loaded, 'SHA-256')
+
+    return signature.hex()
 
 # Example usage
 text = "ESTAMOS EN CLASE DE CRIPTOGRAFIA"
@@ -37,4 +30,4 @@ print("Public key: \n", public_key.save_pkcs1().decode())
 
 # Sign the message
 signature = sign_message(text, private_key)
-print("Firma con RSA: \n", signature)
+print("Firma con RSA en hexadecimal: \n", signature)
